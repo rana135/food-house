d55dc4149d40d75b4785454f5df2679c6ff5a27e
rana135 <rana286090@gmail.com>
2022-04-04 04:45:01 +0600

    dashboard done

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, PieChart, Pie } from 'recharts';
 
 const Dashboard = () => {
     const data = [
@@ -40,27 +40,97 @@ const Dashboard = () => {
             "revenue": 61000
         }
     ];
+
+    const data01 = [
+        // { name: 'Group A', value: 400 },
+        // { name: 'Group B', value: 300 },
+        // { name: 'Group C', value: 300 },
+        // { name: 'Group D', value: 200 },
+        // { name: 'Group E', value: 278 },
+        // { name: 'Group F', value: 189 },
+        {
+            "month": "Mar",
+            "investment": 100000,
+            "sell": 241,
+            "revenue": 10401
+        },
+        {
+            "month": "Apr",
+            "investment": 200000,
+            "sell": 423,
+            "revenue": 24500
+        },
+        {
+            "month": "May",
+            "investment": 500000,
+            "sell": 726,
+            "revenue": 67010
+        },
+        {
+            "month": "Jun",
+            "investment": 500000,
+            "sell": 529,
+            "revenue": 40405
+        },
+        {
+            "month": "Jul",
+            "investment": 600000,
+            "sell": 601,
+            "revenue": 50900
+        },
+        {
+            "month": "Aug",
+            "investment": 700000,
+            "sell": 670,
+            "revenue": 61000
+        }
+    ];
+
     return (
         <div>
-            <BarChart
-          width={500}
-          height={300}
-          data={data}
-          margin={{
-            top: 5,
-            right: 30,
-            left: 20,
-            bottom: 5,
-          }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="month" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Bar dataKey="revenue" fill="#8884d8" />
-          <Bar dataKey="investment" fill="#82ca9d" />
-        </BarChart>
+            <h1 style={{ marginLeft: "70px" }}>Investment VS Revenue</h1>
+            <div>
+                <BarChart
+                    width={500}
+                    height={300}
+                    data={data}
+                    margin={{
+                        top: 5,
+                        right: 30,
+                        left: 20,
+                        bottom: 5,
+                    }}
+                >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="month" />
+                    <YAxis />
+                    <Tooltip />
+                    <Legend />
+                    <Bar dataKey="revenue" fill="#8884d8" />
+                    <Bar dataKey="investment" fill="#82ca9d" />
+                </BarChart>
+            </div>
+
+            {/* Pie Chart */}
+            <div>
+                <h1 style={{ marginLeft: "70px", marginTop: "150px", lineHeight: "5px" }}>Investment VS Revenue</h1>
+                <div>
+                    <PieChart width={400} height={400}>
+                        <Pie
+                            dataKey="revenue"
+                            isAnimationActive={false}
+                            data={data01}
+                            cx="50%"
+                            cy="50%"
+                            outerRadius={80}
+                            fill="#8884d8"
+                            label
+                        />
+                        <Pie dataKey="value" data={data01} cx={500} cy={200} innerRadius={40} outerRadius={80} fill="#82ca9d" />
+                        <Tooltip />
+                    </PieChart>
+                </div>
+            </div>
         </div>
     );
 };
