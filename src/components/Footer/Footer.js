436da8dcc740bc25cc297d69d3d278dfc6360de7
rana135 <rana286090@gmail.com>
2022-04-05 21:28:26 +0600

    project finish

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,9 +4,11 @@ import './Footer.css'
 const Footer = () => {
     return (
         <footer>
+            <div className='foot'>
             <h2>Food House</h2>
             <p className="footvar">Copyright 2022 Food House</p>
             <p className="footvar">All rights reserved</p>
+            </div>
         </footer>
     );
 };
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -18,7 +18,7 @@ const Home = () => {
                         <p>Are you hungry? Did you have a long and stressful day? Interested in getting a cheesy pizza delivered to your office or looking to avoid the weekly shop? Then food house USA is the right destination for you! food house offers you a long and detailed list of the best restaurants and shops near you to help make your everyday easier.</p>
                         <button className='btn'>Live demo</button>
                     </div>
-                    <div>
+                    <div className='bal'>
                         <img className='banner-img' src='https://img.freepik.com/free-photo/penne-pasta-tomato-sauce-with-chicken-tomatoes-wooden-table_2829-19744.jpg?t=st=1649010536~exp=1649011136~hmac=9b2b54c8e30e2081ff18d070c149a178fc8f9447c63efcfb346fc4f2ab381ed1&w=740' alt=''></img>
                     </div>
                 </div>
